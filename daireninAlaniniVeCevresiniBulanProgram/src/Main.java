0d29e9b0d72c8d0cd43e471f5d27ab43e99a4eaa
unknown <atilla.kalayy@gmail.com>
2022-09-29 14:57:37 +0300

    Homework complated.

--- daireninAlaniniVeCevresiniBulanProgram/src/Main.java
+++ daireninAlaniniVeCevresiniBulanProgram/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int r;
+        double pi = 3.14;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Dairenin yarı çapını Giriniz: ");
+        r = scanner.nextInt();
+
+        double alan = pi * r * r;
+        double cevre = 2 * r * pi;
+
+        System.out.println("Dairenin alanı: " + alan);
+        System.out.println("Dairenin Çevresi: " + cevre);
     }
 }
