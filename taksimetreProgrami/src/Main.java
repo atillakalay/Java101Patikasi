f944e46e0e562045822fba5e59bb124d2c7a27fe
unknown <atilla.kalayy@gmail.com>
2022-09-29 14:50:51 +0300

    Homework complated.

--- taksimetreProgrami/src/Main.java
+++ taksimetreProgrami/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double tutar, kmBasiUcret = 2.20, km, baslangicFiyati = 10;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Uzaklık kaç km? ");
+        km = scanner.nextDouble();
+        tutar = (km * kmBasiUcret) + baslangicFiyati;
+
+        if (km < 1) {
+            System.out.println("Ödenecek tutar: " + baslangicFiyati);
+        } else if (tutar <= 20) {
+            tutar = 20;
+            System.out.println("Ödenecek tutar: " + tutar);
+        } else {
+            System.out.println("Ödenecek tutar: " + tutar);
+        }
     }
 }
